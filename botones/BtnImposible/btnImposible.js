710f73006b1c6815cbdc02c1ad8f51fe3f3bfb19
neytor <ortegajoseweb02@gmail.com>
2024-03-21 21:16:13 -0300

    final touches

--- botones/BtnImposible/btnImposible.js
+++ botones/BtnImposible/btnImposible.js
@@ -1,9 +1,10 @@
 const btnImposible = document.getElementById("btn__imposible");
 function moveBTN(e) {
-  let x = Math.random() * window.innerWidth;
-  let y = Math.random() * window.innerHeight;
+  let x = Math.random(0, 0.5) * window.innerWidth;
+  let y = Math.random(0, 0.5) * window.innerHeight;
 
   btnImposible.style.position = "absolute";
+
   btnImposible.style.left = x + "px";
   btnImposible.style.top = y + "px";
 }
